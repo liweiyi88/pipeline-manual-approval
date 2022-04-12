b02d396eb14d661bab8440a7193965bf3d79f216
Julian Li <julian.li@iag.com.au>
2022-04-12 14:52:49 +1000

    include real cdk app

---
+++ lib/pipeline-manual-approval-stack.ts
@@ -0,0 +1,17 @@
+import {Duration, Stack, StackProps} from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import {Queue} from "aws-cdk-lib/aws-sqs";
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class PipelineManualApprovalStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+
+    // example resource
+    new Queue(this, 'PipelineManualApprovalQueue', {
+      visibilityTimeout: Duration.seconds(300)
+    });
+  }
+}
---
+++ test/pipeline-manual-approval.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as PipelineManualApproval from '../lib/pipeline-manual-approval-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/pipeline-manual-approval-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new PipelineManualApproval.PipelineManualApprovalStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
