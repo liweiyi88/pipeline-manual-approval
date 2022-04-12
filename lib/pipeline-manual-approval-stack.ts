5b6a112bd13e941fd589f164d50d047268ee72e6
Julian Li <julian.li@iag.com.au>
2022-04-12 15:51:46 +1000

    fixes

--- lib/pipeline-manual-approval-stack.ts
+++ lib/pipeline-manual-approval-stack.ts
@@ -1,12 +1,10 @@
 import {Duration, Stack, StackProps} from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import {Queue} from "aws-cdk-lib/aws-sqs";
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class PipelineManualApprovalStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
-
 
     new Queue(this, 'PipelineManualApprovalQueue', {
       visibilityTimeout: Duration.seconds(300)
